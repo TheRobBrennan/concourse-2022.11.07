c09229dc14c54762b8a09eb47dcb1de498c8023f
Rob Brennan <rob@therobbrennan.com>
2022-11-09 22:54:42 -0800

    Add styling to our post detail page

--- __reference__/nextjs-blog/pages/posts/[id].js
+++ __reference__/nextjs-blog/pages/posts/[id].js
@@ -1,8 +1,10 @@
 import Head from "next/head";
 import Layout from "../../components/layout";
 import Date from "../../components/date";
-
 import { getAllPostIds, getPostData } from "../../lib/posts";
+
+// Styling
+import utilStyles from "../../styles/utils.module.css";
 
 // Create an array of possible paths for our slugs
 export async function getStaticPaths() {
@@ -28,19 +30,16 @@ export async function getStaticProps({ params }) {
 export default function Post({ postData }) {
   return (
     <Layout>
-      {/* Add this <Head> tag */}
       <Head>
         <title>{postData.title}</title>
       </Head>
-
-      {/* Keep the existing code here */}
-      {postData.title}
-      <br />
-      {postData.id}
-      <br />
-      <Date dateString={postData.date} />
-      <br />
-      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
     </Layout>
   );
 }

4c557172421028bd01d764028de0658e7e91dd57
Rob Brennan <rob@therobbrennan.com>
2022-11-09 22:01:48 -0800

    Add a commented example demonstrating how server-side rendering might be used instead of static generation

--- __reference__/nextjs-blog/pages/index.js
+++ __reference__/nextjs-blog/pages/index.js
@@ -25,6 +25,23 @@ export async function getStaticProps() {
   };
 }
 
+// export async function getServerSideProps(context) {
+//   //  To use server-side rendering, we would need to use getServerSideProps
+//   //  Because getServerSideProps is called at request time, its parameter (context) contains
+//   //  request specific parameters.
+//   //
+//   //  You should use getServerSideProps only if you need to pre-render a page whose
+//   //  data must be fetched at request time. Time to first byte (TTFB) will be slower than
+//   //  getStaticProps because the server must compute the result on every request, and the
+//   //  result cannot be cached by a CDN without extra configuration.
+
+//   return {
+//     props: {
+//       // props for your component
+//     },
+//   };
+// }
+
 export default function Home({ allPostsData }) {
   return (
     <Layout home>
